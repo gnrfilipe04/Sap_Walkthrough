0e6d9ba5cca0681e971b9a0cc26463836c02faee
Filipe <flpfilipe19@gmail.com>
2021-01-04 14:38:46 -0300

    first commit

---
+++ controller/Detail.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+	"../model/formatter"
+], function (Controller, UIComponent, History, JSONModel, MessageToast, formatter) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+		
+		formatter: formatter,
+		onInit: function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+				
+				var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			
+			this.getView().setModel(oViewModel, "view");
+			
+		},
+		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
+			this.getView().bindElement({
+				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTo("overview", {}, true);
+			}
+		},
+
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
+	});
+});
